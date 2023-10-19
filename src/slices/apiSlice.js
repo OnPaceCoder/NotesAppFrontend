9614922dad50e6d5e95e5db7c5f4dfbe1c91d86a
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-19 21:30:47 +0530

    Add: apiSlice

---
+++ src/slices/apiSlice.js
@@ -0,0 +1,15 @@
+import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
+
+
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: "http://localhost:5000",
+
+});
+
+
+export const apiSlice = createApi({
+    baseQuery,
+    tagTypes: ['notes'],
+    endpoints: (builder) => ({}),
+})
