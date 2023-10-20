41b7d130c8440eb486db30dd7280faa8177cead3
Priyank Vasoya <priyankvasoya7@gmail.com>
2023-10-20 22:19:17 +0530

    fix: removed tanstack

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import store from './store.js'
-const queryClient = new QueryClient()
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <QueryClientProvider client={queryClient}>
 
-    <Provider store={store}>
+  <Provider store={store}>
 
-      <App />
-    </Provider>
-  </QueryClientProvider>
+    <App />
+  </Provider>
+
 
 )
